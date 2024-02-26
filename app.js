1c4b16775b4f72d4c67972cca0548655a66730a6
gotax24 <dev.ejbr@gmail.com>
2024-02-26 16:24:23 -0300

    Cambiar las variables al ingles y resetear los input

--- app.js
+++ app.js
@@ -1,99 +1,125 @@
-let notas = [];
+let notes = [];
 
-const agregar = () => {
-  let notita = document.getElementById("nota").value;
-  if (notita === "") {
+//Resetear los input despues de usarlos
+const resetInput = () => {
+  const add = document.getElementById("note");
+  const edit = document.getElementById("edit");
+  const numberEdit = document.getElementById("number-edit");
+  const eliminate = document.getElementById("eliminate");
+
+  let a = (add.value = "");
+  let b = (edit.value = "");
+  let c = (numberEdit.value = "");
+  let d = (eliminate.value = "");
+
+  return a, b, c, d;
+};
+
+// Funcion que agrega las notas al array
+const add = () => {
+  let info = document.getElementById("note").value;
+  if (info === "") {
     alert("Esta vacia la nota");
   } else {
-    notas.push(notita);
-    guardarNotasEnLocalStorage(); // Guarda las notas actualizadas en localStorage
+    notes.push(info);
+    saveNotes(); // Guarda las notas actualizadas en localStorage
   }
-  ver();
+  view();
+  resetInput();
 };
 
-const ver = () => {
-  let leer = document.getElementById("lista");
-  leer.innerHTML = "";
-  for (let i = 0; i < notas.length; i++) {
-    leer.innerHTML += `<li>${notas[i]}</li>`;
+//Funcion para mostrar los datos guardados
+const view = () => {
+  let read = document.getElementById("list");
+  read.innerHTML = "";
+  for (let i = 0; i < notes.length; i++) {
+    read.innerHTML += `<li>${notes[i]}</li>`;
   }
 };
 
-const editar = () => {
+//Funcion para editar los datos en el array
+const edit = () => {
   let input = document.createElement("input");
   input.type = "number";
   input.placeholder = "Cual numero quieres editar?";
-  input.classList.add("dato");
+  input.classList.add("datum");
+  input.id = "number-edit";
 
-  let edicion = document.createElement("input");
-  edicion.type = "text";
-  edicion.placeholder = "Nueva nota";
-  edicion.classList.add("dato");
+  let edit = document.createElement("input");
+  edit.type = "text";
+  edit.placeholder = "Nueva nota";
+  edit.classList.add("datum");
+  edit.id = "edit";
 
-  let boton2 =
-    document.getElementById("cambio") || document.createElement("button");
-  if (!boton2.id) {
-    boton2.id = "cambio";
-    boton2.textContent = "Confirmar cambio";
-    boton2.classList.add("boton");
-    let contenedor = document.getElementById("contenedor");
-    contenedor.appendChild(input);
-    contenedor.appendChild(edicion);
-    contenedor.appendChild(boton2);
+  //Se pregunta si en el document esta el id si no esta se crea ...
+  let button =
+    document.getElementById("change") || document.createElement("button");
+  if (!button.id) {
+    button.id = "change";
+    button.textContent = "Confirmar el cambio";
+    button.classList.add("boton");
+    let container = document.getElementById("container");
+    container.appendChild(input);
+    container.appendChild(edit);
+    container.appendChild(button);
 
-    boton2.addEventListener("click", () => {
+    button.addEventListener("click", () => {
       let indice = parseInt(input.value) - 1;
-      let nuevaNota = edicion.value;
-      if (indice < 0 || indice >= notas.length) {
+      let nuevaNota = edit.value;
+      if (indice < 0 || indice >= notes.length) {
         alert("El indice no existe");
       } else if (nuevaNota === "") {
         alert("La nota esta vacia");
       } else {
-        notas[indice] = nuevaNota;
-        ver();
-        guardarNotasEnLocalStorage()
+        notes[indice] = nuevaNota;
+        view();
+        saveNotes();
+        resetInput();
       }
     });
   }
 };
 
-const eliminar = () => {
+const eliminate = () => {
   let input = document.createElement("input");
   input.type = "number";
   input.placeholder = "Cual  numero quieres eliminar?";
-  input.classList.add("dato");
+  input.classList.add("datum");
+  input.id = "eliminate";
 
-  let boton2 =
+  //Se pregunta si en el document esta el id si no esta se crea ...
+  let button =
     document.getElementById("confirmar") || document.createElement("button");
-  if (!boton2.id) {
-    boton2.textContent = "Confirmar eliminacion";
-    boton2.id = "confirmar";
-    boton2.classList.add("boton");
+  if (!button.id) {
+    button.textContent = "Confirmar";
+    button.id = "confirmar";
+    button.classList.add("boton");
 
-    let contendeor = document.getElementById("contenedor");
-    contendeor.appendChild(input);
-    contendeor.appendChild(boton2);
+    let container = document.getElementById("container");
+    container.appendChild(input);
+    container.appendChild(button);
 
-    boton2.addEventListener("click", () => {
+    button.addEventListener("click", () => {
       let number = input.value - 1;
-      if (number < 0 || number >= notas.length) {
+      if (number < 0 || number >= notes.length) {
         alert("El indice no existe");
       } else {
-        notas.splice(number, 1);
-        ver();
-        guardarNotasEnLocalStorage()
+        notes.splice(number, 1);
+        view();
+        saveNotes();
+        resetInput();
       }
     });
   }
 };
 
-//Verifica el local storage
-if (localStorage.getItem("notitas")) {
-  notas = JSON.parse(localStorage.getItem("notitas"));
-  ver();
+//verifica el local storage
+if (localStorage.getItem("note")) {
+  notes = JSON.parse(localStorage.getItem("note"));
+  view();
 }
 
-const guardarNotasEnLocalStorage = () => {
+const saveNotes = () => {
   // Guarda las notas en el localStorage en formato JSON
-  localStorage.setItem("notitas", JSON.stringify(notas));
+  localStorage.setItem("note", JSON.stringify(notes));
 };
